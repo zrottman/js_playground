8ad152d2554062c21fd11b15bcd07c0f49f9d39c
zrottman <zrottman@example.com>
2023-04-12 11:07:28 -0700

    First commit of BPM counter based on simple counter with working average computation

---
+++ 02_BPM-counter/resources/index.js
@@ -0,0 +1,30 @@
+let counterDisplay = document.getElementById("display");
+let count = 0;
+let curTime;
+let elapsedTime;
+let lastTime;
+let avg = 0;
+
+function beat() {
+  
+  curTime = Date.now();
+
+  if (lastTime) { // not first button press
+    elapsedTime = curTime - lastTime;
+    avg = (avg * (count - 1) + elapsedTime) / count;
+  }
+
+  lastTime = curTime;  
+  count ++;
+
+  counterDisplay.textContent = count;
+  console.log("Count: " + count);
+  console.log("Cur elapsed: " + elapsedTime);
+  console.log("Cur avg: " + avg);
+}
+
+function reset() {
+  count = 0;
+  counterDisplay.textContent = count;
+  startTime = null;
+}

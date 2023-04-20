d866964b9ee1e0ba4efba341a68355f57a5ef226
zrottman <zrottman@example.com>
2023-04-20 11:03:59 -0700

    Refactor and simplify js; implement working radio buttons

--- 03_bitwise-operations/resources/index.js
+++ 03_bitwise-operations/resources/index.js
@@ -1,22 +1,34 @@
-const byteDivs = document.querySelectorAll(".byte");
+// Arrays to hold states of input and output bytes
 let byteA = [0, 0, 0, 0, 0, 0, 0, 0];
 let byteB = [0, 0, 0, 0, 0, 0, 0, 0];
 let byteY = [0, 0, 0, 0, 0, 0, 0, 0];
+
+// Byte divs
+const byteDivs = document.querySelectorAll(".byte");
 const byteYBits = document.querySelector('.byte-Y').children;
+
+// Binary and integer divs for rendering
+const byteABinary = document.querySelector(".binary.byte-A");
+const byteBBinary = document.querySelector(".binary.byte-B");
+const byteYBinary = document.querySelector(".binary.byte-Y");
+
+const byteAInt= document.querySelector(".integer.byte-A");
+const byteBInt= document.querySelector(".integer.byte-B");
+const byteYInt= document.querySelector(".integer.byte-Y");
+
+// Operator buttons
+const operatorBtns = document.getElementById("operators");
+operatorBtns.addEventListener('change', () => {
+  render();
+});
+
 
 // Create byte divs and 8 bits for each
 for (let byteDiv of byteDivs) {
-
   for (let i=0; i < 8; i++) {
-
-    // create button el
     const button = document.createElement("button");
-
-    // add classes
     button.classList.add("bit");
     button.classList.add(`bit-${i}`);
-
-    // add event listeners for input bytes only
     if (byteDiv.classList.contains('input')) {
       button.addEventListener("click", function () {
         this.classList.toggle("toggle");
@@ -25,26 +37,69 @@ for (let byteDiv of byteDivs) {
         } else {
           byteB[i] = byteB[i] ? 0 : 1;
         }
-        updateOutput(i);
+        render();
       });
-
     }
-
-    // append button to parent el
     byteDiv.appendChild(button);
   }
 }
 
-function updateOutput(i) {
-  byteY[i] = byteA[i] & byteB[i];
-  byteYBits[i].classList.toggle("toggle", byteY[i] == 1);
-  console.log(byteY);
+render();
+
+
+function render() {
+  updateByteY();
+  updateByteYButtons();
+  renderBinary();
+  renderInteger();
 }
 
-function arrToBinStr(arr) {
-  return arr.join('');
+function updateByteY() {
+  let byteAInt = binArrToInt(byteA);
+  let byteBInt = binArrToInt(byteB);
+  let operator = document.querySelector('input[name="operator"]:checked').value;
+
+  if (operator === 'AND') {
+    byteY = intToBinArr(byteAInt & byteBInt);
+  } else if (operator === 'OR') {
+    byteY = intToBinArr(byteAInt | byteBInt);
+  } else if (operator === 'XOR') {
+    byteY = intToBinArr(byteAInt ^ byteBInt);
+  }
+}
+
+function updateByteYButtons() {
+  for (let i = 0; i < 8; i++) {
+    byteYBits[i].classList.toggle("toggle", byteY[i] == 1);
+  }
+}
+
+function renderBinary() {
+  byteABinary.textContent = binArrToBinStr(byteA);
+  byteBBinary.textContent = binArrToBinStr(byteB);
+  byteYBinary.textContent = binArrToBinStr(byteY);
+}
+
+function renderInteger() {
+  byteAInt.textContent = binArrToInt(byteA);
+  byteBInt.textContent = binArrToInt(byteB);
+  byteYInt.textContent = binArrToInt(byteY);
+}
+
+function intToBinArr(num) {
+  b = num.toString(2);
+  b = '00000000'.substr(b.length) + b;
+  return b.split('');
+}
+
+function binArrToBinStr(binArr) {
+  return binArr.join('');
 }
 
 function binStrToInt(binStr) {
-  return Number('0b' + binStr);
+  return Number(`0b${binStr}`);
+}
+
+function binArrToInt(binArr) {
+  return binStrToInt(binArrToBinStr(binArr));
 }
